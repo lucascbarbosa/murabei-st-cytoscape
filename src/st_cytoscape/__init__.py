c2460c910ec468148be2b6056e64767fe418f7de
Lucas Barbosa <lucas.barbosa@murabei.com>
2024-11-05 20:16:11 -0300

    Building a new version 0.7.4

--- src/st_cytoscape/__init__.py
+++ src/st_cytoscape/__init__.py
@@ -1,12 +1,13 @@
 import os
 import streamlit.components.v1 as components
 
-_RELEASE = True
+_RELEASE = False
 
 if not _RELEASE:
     _component_func = components.declare_component(
         "st_cytoscape", url="http://localhost:3001",
     )
+
 else:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
