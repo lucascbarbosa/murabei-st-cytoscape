c893ba9e3ad9dfb40d1317a3c7a2454ee2892126
Lucas Barbosa <lucas.barbosa@murabei.com>
2024-11-05 19:37:42 -0300

    Building a new version 0.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name='murabei-st-cytoscape',
-    version='0.3',
+    version='0.4',
     include_package_data=True,
     license='BSD-3-Clause License',
     description=(
