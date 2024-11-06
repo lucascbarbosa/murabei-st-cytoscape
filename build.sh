4bb547bba9cc0a0f65114d725b85cfc49a663638
Lucas Barbosa <lucas.barbosa@murabei.com>
2024-11-06 10:49:35 -0300

    rebuilding image new version 0.7.7

--- build.sh
+++ build.sh
@@ -1,21 +1,26 @@
 # Set version
 source version
 sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
-sed -e 's#{VERSION}#'"${VERSION}"'#g' st_cytoscape/frontend/package-template.json > st_cytoscape/frontend/package.json 
+sed -e 's#{VERSION}#'"${VERSION}"'#g' murabei_st_cytoscape/frontend/package-template.json > murabei_st_cytoscape/frontend/package.json 
 
-# Build component
-cd st_cytoscape/frontend
-npm install
-npm audit fix
-npm run build
+# # Build component
+# cd st_cytoscape/frontend
+# rm -rf build/
+# rm -rf node_modules/
+# rm -rf murabei_st_cytoscape.egg-info/
+# npm install
+# npm audit fix
+# npm run build
 
-# Build wheel
-cd ../..
-python3 setup.py sdist bdist_wheel
+# # Build wheel
+# cd ../..
+# rm -rf build/
+# rm -rf dist/
+# python3 setup.py sdist bdist_wheel
 
-# Upload to PyPI
-python -m twine upload --repository testpypi dist/*
+# # Upload to PyPI
+# python -m twine upload --repository testpypi dist/*
 
-# Commit all
-git add --all
-git commit -m "Building a new version ${VERSION}"
+# # Commit all
+# git add --all
+# git commit -m "Building a new version ${VERSION}"
