a07e365c06f6b790fb7a17d5f59bc819e405356e
Lucas Barbosa <lucas.barbosa@murabei.com>
2024-11-06 10:54:14 -0300

    removed gitignore

---
+++ murabei_st_cytoscape/frontend/build/precache-manifest.60b056ed3cadeba790594adc9af238f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff4fbf144f07bd0abe8e71d9bc5ccbc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28419a08b40b481eca15",
+    "url": "./static/js/2.cf0c91f1.chunk.js"
+  },
+  {
+    "revision": "12ecc5f2f0d11f924dde9479673b3a5b",
+    "url": "./static/js/2.cf0c91f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa74dc32779bfb24eed8",
+    "url": "./static/js/main.8dc26177.chunk.js"
+  },
+  {
+    "revision": "3e279e2f4778cdbc58e4",
+    "url": "./static/js/runtime-main.9d57f012.js"
+  }
+]);
