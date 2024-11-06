244898402b7808e4ccadd0b4040f5ce9e4de6c91
Lucas Barbosa <lucas.barbosa@murabei.com>
2024-11-06 10:59:03 -0300

    Building a new version 0.7.8

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ source version
 sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
 sed -e 's#{VERSION}#'"${VERSION}"'#g' murabei_st_cytoscape/frontend/package-template.json > murabei_st_cytoscape/frontend/package.json 
 
-# # Build component
-# cd st_cytoscape/frontend
-# rm -rf build/
-# rm -rf node_modules/
-# rm -rf murabei_st_cytoscape.egg-info/
+# Build component
+cd st_cytoscape/frontend
+rm -rf build/
+rm -rf node_modules/
+rm -rf murabei_st_cytoscape.egg-info/
 # npm install
 # npm audit fix
 # npm run build
