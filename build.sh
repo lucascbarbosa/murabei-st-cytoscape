2934f99ce4da375b502735b956979c68829a9061
Lucas Barbosa <lucas.barbosa@murabei.com>
2024-11-29 11:29:00 -0300

    final commit

--- build.sh
+++ build.sh
@@ -4,24 +4,24 @@ sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.py > setup.py
 sed -e 's#{VERSION}#'"${VERSION}"'#g' murabei_st_cytoscape/frontend/package-template.json > murabei_st_cytoscape/frontend/package.json 
 
 # Build component
-cd st_cytoscape/frontend
+cd murabei_st_cytoscape/frontend
 rm -rf build/
 rm -rf node_modules/
-rm -rf murabei_st_cytoscape.egg-info/
-# npm install
-# npm audit fix
-# npm run build
+npm install
+npm audit fix
+npm run build
 
 # # Build wheel
-# cd ../..
-# rm -rf build/
-# rm -rf dist/
+cd ../..
+rm -rf build/
+rm -rf dist/
+rm -rf murabei_st_cytoscape.egg-info/
 # python3 setup.py sdist bdist_wheel
 
 # # Upload to PyPI
 # python -m twine upload --repository testpypi dist/*
 
-# Commit all
-git add --all
-git commit -m "Building a new version ${VERSION}"
-git push
+# # Commit all
+# git add --all
+# git commit -m "Building a new version ${VERSION}"
+# git push
---
+++ build/lib/murabei_st_cytoscape/frontend/build/precache-manifest.fc7d7b79ad4f645da0f76f82754a60d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a504f177f69cdab5bcb86be1b26fd71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3768fe49e8ae9dbb026b",
+    "url": "./static/js/2.2bf01589.chunk.js"
+  },
+  {
+    "revision": "12ecc5f2f0d11f924dde9479673b3a5b",
+    "url": "./static/js/2.2bf01589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aad11c8defbaaceff45",
+    "url": "./static/js/main.401dc821.chunk.js"
+  },
+  {
+    "revision": "3e279e2f4778cdbc58e4",
+    "url": "./static/js/runtime-main.9d57f012.js"
+  }
+]);
